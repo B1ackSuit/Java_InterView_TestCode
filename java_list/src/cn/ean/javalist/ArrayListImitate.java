9d1fe2a180d4db34c4f506e1a5c388456a3ec88c
BlackSuit <ean969648839@163.com>
2021-09-17 23:55:54 +0800

    HashMap Constructor

--- java_list/src/cn/ean/javalist/ArrayListImitate.java
+++ java_list/src/cn/ean/javalist/ArrayListImitate.java
@@ -1,6 +1,7 @@
 package cn.ean.javalist;
 
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 import java.util.*;
 
--- java_list/src/cn/ean/javalist/HashMapImitate.java
+++ java_list/src/cn/ean/javalist/HashMapImitate.java
@@ -1,10 +1,11 @@
 package cn.ean.javalist;
 
+import org.junit.platform.engine.support.hierarchical.Node;
+
 import java.io.Serializable;
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
 
 /**
  * FileName:HashMapImitate
@@ -13,6 +14,149 @@ import java.util.Set;
  **/
 public class HashMapImitate<K,V> extends AbstractMap<K,V>
         implements Map<K,V>, Cloneable, Serializable {
+
+
+    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    static final int TREEIFY_THRESHOLD = 8;
+
+    static final int UNTREEIFY_THRESHOLD = 6;
+
+    static class Node<K,V> implements Map.Entry<K,V> {
+        final int hash;
+        final K key;
+        V value;
+        Node<K,V> next;
+
+        Node(int hash, K key, V value, Node<K,V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public final K getKey()        { return key; }
+        public final V getValue()      { return value; }
+        public final String toString() { return key + "=" + value; }
+
+        public final int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
+        }
+
+        public final V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+
+        public final boolean equals(Object o) {
+            if (o == this)
+                return true;
+            if (o instanceof Map.Entry) {
+                Map.Entry<?,?> e = (Map.Entry<?,?>)o;
+                if (Objects.equals(key, e.getKey()) &&
+                        Objects.equals(value, e.getValue()))
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    static final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    static Class<?> comparableClassFor(Object x) {
+        if (x instanceof Comparable) {
+            Class<?> c; Type[] ts, as; ParameterizedType p;
+            if ((c = x.getClass()) == String.class) // bypass checks
+                return c;
+            if ((ts = c.getGenericInterfaces()) != null) {
+                for (Type t : ts) {
+                    if ((t instanceof ParameterizedType) &&
+                            ((p = (ParameterizedType) t).getRawType() ==
+                                    Comparable.class) &&
+                            (as = p.getActualTypeArguments()) != null &&
+                            as.length == 1 && as[0] == c) // type arg is c
+                        return c;
+                }
+            }
+        }
+        return null;
+    }
+
+    static int compareComparables(Class<?> kc, Object k, Object x) {
+        return (x == null || x.getClass() != kc ? 0 :
+                ((Comparable)k).compareTo(x));
+    }
+
+    static final int tableSizeFor(int cap) {
+        int n = -1 >>> Integer.numberOfLeadingZeros(cap - 1);
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    transient Node<K,V>[] table;
+
+    transient Set<Map.Entry<K,V>> entrySet;
+
+    transient int size;
+
+    transient int modCount;
+
+    int threshold;
+
+    final float loadFactor;
+
+    public HashMapImitate(int initialCapacity, float loadFactor) {
+        if (initialCapacity < 0)
+            throw new IllegalArgumentException("Illegal initial capacity: " +
+                    initialCapacity);
+        if (initialCapacity > MAXIMUM_CAPACITY)
+            initialCapacity = MAXIMUM_CAPACITY;
+        if (loadFactor <= 0 || Float.isNaN(loadFactor))
+            throw new IllegalArgumentException("Illegal load factor: " +
+                    loadFactor);
+        this.loadFactor = loadFactor;
+        this.threshold = tableSizeFor(initialCapacity);
+    }
+
+    public HashMapImitate(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
+    }
+
+    public HashMapImitate() {
+        this.loadFactor = DEFAULT_LOAD_FACTOR; // all other fields defaulted
+    }
+
+//    public HashMapImitate(Map<? extends K, ? extends V> m) {
+//        this.loadFactor = DEFAULT_LOAD_FACTOR;
+//        putMapEntries(m, false);
+//    }
+//
+//    final void putMapEntries(Map<? extends K, ? extends V> m, boolean evict) {
+//        int s = m.size();
+//        if (s > 0) {
+//            if (table == null) { // pre-size
+//                float ft = ((float)s / loadFactor) + 1.0F;
+//                int t = ((ft < (float)MAXIMUM_CAPACITY) ?
+//                        (int)ft : MAXIMUM_CAPACITY);
+//                if (t > threshold)
+//                    threshold = tableSizeFor(t);
+//            }
+//            else if (s > threshold)
+//                resize();
+//            for (Map.Entry<? extends K, ? extends V> e : m.entrySet()) {
+//                K key = e.getKey();
+//                V value = e.getValue();
+//                putVal(hash(key), key, value, false, evict);
+//            }
+//        }
+//    }
 
 
 
