0fc5ee8573b0a4fb2d8852f2bd1995d3a84b2468
BlackSuit <ean969648839@163.com>
2021-09-20 20:44:57 +0800

    0920 beta

--- java_list/src/cn/ean/javalist/HashMapImitate.java
+++ java_list/src/cn/ean/javalist/HashMapImitate.java
@@ -445,13 +445,6 @@ public class HashMapImitate<K,V> extends AbstractMap<K,V>
     }
 
     public static void main(String[] args) {
-        HashMapImitate<String, String> o = new HashMapImitate<>();
-        o.put("aaaa", "aaaa");
-        o.put("aaab", "aaab");
-        o.put("aaac", "aaac");
-        o.put("aaad", "aaad");
-        System.out.println( "debug:aaaaa:" +
-        HashMapImitate.hash("aaaaa")
-        );
+
     }
 }
